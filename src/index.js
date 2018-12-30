fe844944aced4bff76172181ce4b2cb832e412a7
KelliePetersen <KellieAnnPetersen@gmail.com>
2018-12-29 21:04:27 -0500

    update readme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+const greeting = React.createElement('h1', {}, 'Hello world');
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
